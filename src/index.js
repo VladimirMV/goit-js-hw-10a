6ed15ad0fa2efec898e2b4cf7f5371c6ea212cea
Vladimir Minakov <Vladimirmvk@gmail.com>
2023-02-08 15:01:19 +0200

    77

--- src/index.js
+++ src/index.js
@@ -1,3 +1,87 @@
 import './css/styles.css';
 
+import _debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import fetchCountries from './js/fetchCountries.js';
+
+import templateCountryList from './templates/country-list.hbs';
+import templateCountryInfo from './templates/country-info.hbs';
+
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  inputEl: document.querySelector('#search-box'),
+  countryListEl: document.querySelector('.country-list'),
+  countryInfoEl: document.querySelector('.country-info'),
+};
+
+refs.inputEl.addEventListener(
+  'input',
+  _debounce(onSearchCountryInput, DEBOUNCE_DELAY)
+);
+
+const clearMarkup = element => (element.innerHTML = '');
+const changeBorderColor = color => (refs.inputEl.style.backgroundColor = color);
+
+function onSearchCountryInput(event) {
+  clearMarkup(refs.countryListEl);
+  clearMarkup(refs.countryInfoEl);
+
+  changeBorderColor('white');
+
+  if (!event.target.value.trim()) {
+    return;
+  }
+
+  fetchCountries(event.target.value.trim())
+    .then(countries => { 
+
+        console.log(countries);
+    console.log(event.target.value.trim());
+
+      if (countries.length > 10) {
+        Notify.info(
+          '⚠️Too many matches found. Please enter a more specific name.'
+        );
+        changeBorderColor('lightblue');
+        return;
+      }
+      renderMarkup(countries);
+    });
+    // .catch(() => {
+    //   Notify.failure('❌Oops, there is no country with that name');
+    //   changeBorderColor('lightcoral');
+    // });
+}
+
+function renderMarkup(countries) {
+  changeBorderColor('khaki');
+
+  let markupInfo = '';
+  let markupList = '';
+
+  if (countries.length >= 2) {
+    markupList = countries.reduce(
+      (previousValue, currentValue) =>
+        (previousValue += templateCountryList(currentValue)),
+      ''
+    );
+  } else {
+    
+    markupList = templateCountryList(...countries);
+    console.log("markupList = ", markupList);
+    console.log(" Делаем markupInfo =" ,...countries);
+    markupInfo = templateCountryInfo(...countries);
+    
+
+    console.log("markupInfo =",markupInfo);
+    changeBorderColor('lightgreen');
+
+    refs.countryInfoEl.insertAdjacentHTML('afterbegin', markupInfo);
+  }
+
+  refs.countryListEl.insertAdjacentHTML('afterbegin', markupList);
+}
+
+ 
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,16 @@
+const BASE_API = 'https://restcountries.com/v3.1/';
+const SEARCH_BY_NAME = `name/`;
+const FIELDS = `?fields=name,capital,population,flags,languages`;
+
+export default function fetchCountries(name) {
+  return fetch(`${BASE_API}${SEARCH_BY_NAME}${name}${FIELDS}`).then(
+    response => {
+        console.log(response);
+      if (!response.ok) {
+        console.log("Error",response.status);
+        throw new Error(response.status);
+      }
+      return response.json();
+    }
+  );
+}
